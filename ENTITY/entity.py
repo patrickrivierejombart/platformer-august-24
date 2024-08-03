e79b43cbe3fd17df4cce6f6d29d727c3caaf0ad1
Wolfur1 <darkwolf.redwolf@gmail.com>
2024-08-03 17:23:57 +0200

    added directions to entity class

--- ENTITY/entity.py
+++ ENTITY/entity.py
@@ -1,5 +1,41 @@
 # Class for all entities
 
 class Entity():
-    def __init__(self):
-        pass
+
+    class Direction():
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+        @property
+        def x(self) -> int:
+            return self.x
+        @property
+        def y(self) -> int:
+            return self.y
+        def update(self, x, y):
+            self.x = x
+            self.y = y
+        def updateX(self, x: int):
+            self.x = x
+        def updateY(self, y: int):
+            self.y = y
+        def right(self):
+            self.x = 1
+        def left(self):
+            self.x = -1
+        def up(self):
+            self.y = 1
+        def down(self):
+            self.y = -1
+        def inactive(self):
+            self.x = 0
+            self.y = 0
+
+    def __init__(self, hp: int, lives: int, object_size: list):
+        self.hp = hp
+        self.lives = lives
+        self.direction = self.Direction(0,0)
+        self.hitbox = object_size
+    def dynamics(self):
+        # Not in accord with map update - To edit
+        return {"x": self.direction.x, "y": self.direction.y}
