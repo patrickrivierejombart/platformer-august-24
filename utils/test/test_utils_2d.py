fb12aa06df614de9d0685f8d8c690004b15a597c
Patrick <55240078+jrpprj@users.noreply.github.com>
2024-08-04 04:30:10 -0700

    Update utilities: better position handling

--- utils/test/test_utils_2d.py
+++ utils/test/test_utils_2d.py
@@ -1,9 +1,32 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-from utils.utils_2d import acceleration, Force
+from utils.utils_2d import acceleration, Force, position, Position, Speed
 
 
 if __name__ == '__main__':
-    acc = acceleration(mass=2, forces=[Force(1,0), Force(-99,-1)])
-    print(acc.vector)
+    pos_x0 = Position(10, 10)
+    jump_vector_list = [
+        Force(0, 0),
+        Force(0, -0.5),
+        Force(0, -0.7),
+        Force(0, -0.87),
+        Force(0, -1),
+        Force(0, -0.87),
+        Force(0, -0.7),
+        Force(0, -0.5),
+        Force(0, 0),
+        Force(0, 0.5),
+        Force(0, 0.7),
+        Force(0, 0.87),
+        Force(0, 1),
+        Force(0, 0.87),
+        Force(0, 0.7),
+        Force(0, 0.5),
+        Force(0, 0)
+    ]
+    for jump_vector in jump_vector_list:
+        pos_x0.update_force(force_list=[jump_vector])
+        pos_x0.update_speed(speed_list=[Speed(1, 0)])
+        pos_x0.increment_position(dt = 1)
+        print(pos_x0.vector)
