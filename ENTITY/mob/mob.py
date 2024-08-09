63e40f97edc66120a9afc4442d5819136bf675ed
Patrick <55240078+jrpprj@users.noreply.github.com>
2024-08-09 01:46:44 -0700

    Clean up mob class

--- ENTITY/mob/mob.py
+++ ENTITY/mob/mob.py
@@ -1,33 +1,33 @@
 from ENTITY.entity import Entity
-from utils.utils_2d import Position
 from typing import Literal
+from pygame.math import Vector2
 import math
 
 
 class Mob(Entity):
     sprite_path="assets/textures/mob/"
 
-    def _circular_los(self, player_position: Position, threshold: int):
+    def _circular_los(self, player_position: Vector2, threshold: int):
         diff_x = player_position.x - self.position.x
         diff_y = player_position.y - self.position.y
         err_player_to_mob = math.sqrt(diff_x**2 + diff_y**2)
         return err_player_to_mob <= threshold
 
-    def _vertical_los(self, player_position: Position, threshold: int):
+    def _vertical_los(self, player_position: Vector2, threshold: int):
         x_offset = self.base_size[0]/2
         return player_position.x >= self.position.x - x_offset and\
             player_position.x <= self.position.x + x_offset and\
             player_position.y >= self.position.y - threshold and\
             player_position.y <= self.position.y + threshold
 
-    def _horizontal_los(self, player_position: Position, threshold: int):
+    def _horizontal_los(self, player_position: Vector2, threshold: int):
         y_offset = self.base_size[1]/2
         return player_position.x >= self.position.x - threshold and\
             player_position.x <= self.position.x + threshold and\
             player_position.y >= self.position.y - y_offset and\
             player_position.y <= self.position.y + y_offset
 
-    def los(self, player_position: Position, threshold: int = 5, los_type: Literal["circular", "vertical", "horizontal"] = "circular") -> bool:
+    def los(self, player_position: Vector2, threshold: int = 5, los_type: Literal["circular", "vertical", "horizontal"] = "circular") -> bool:
         if los_type == "circular":
             return self._circular_los(player_position=player_position, threshold=threshold)
         elif los_type == "vertical":
