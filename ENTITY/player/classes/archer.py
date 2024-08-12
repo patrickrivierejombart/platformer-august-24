27603fb658b07ff42f33229a76f5d61b6a146c7f
Wolfur1 <darkwolf.redwolf@gmail.com>
2024-08-12 12:51:18 +0200

    why am I calling Player now ..

--- ENTITY/player/classes/archer.py
+++ ENTITY/player/classes/archer.py
@@ -6,7 +6,7 @@ from pygame.math import Vector2
 
 
 
-class Archer(player.Player()):
+class Archer(Player):
     def __init__(self, player_pos: Vector2,arrow_pos: Vector2):
         super().__init__()
         self.arrow_pos = arrow_pos
